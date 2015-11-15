b637dde943a0759cbdb15b1fa31b690b9f7e3b91
Cole Clifford <cole.cliffy@gmail.com>
2015-11-15 04:57:43 -0500

    Trying to add front end and back end.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
-var socket = io();
+var socket = io(),
+    storyContainer = $("#padded-story-container"),
+    story = $("#story"),
+    storyProgress = $("#progress"),
+    userData = '';
 
 $('form').submit(function(){
   socket.emit('chat message', $('#m').val());
@@ -7,6 +11,24 @@ $('form').submit(function(){
 });
 
 socket.on('onlineUsers', function(users) {
+    
+    console.log(users);
+    
+    userData = JSON.parse(users);
+    
+    var sideBar = $('#side-bar'),
+        stringSection1 = "<div class='name-tag'> <svg fill='grey' id=",
+        stringSection2 = " height='24' width='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'><path d='M0 0h24v24H0z' fill='none'></path></path></svg>",
+        stringSection3 = " <p class='name' style='color: ",
+        stringSection4 = " '> ",
+        stringSection5 = "</p></div>";
+        
+    
+    for(i in users) {
+        var text = stringSection1 + users[i].username + stringSection2 + stringSection3 + users[i].textColor + stringSection4 + users[i].username + stringSection5;
+        sideBar.append(text);
+    }
+    
   //$('#messages').append($('<li>').text(msg));
   /*
 
@@ -24,10 +46,12 @@ socket.on('onlineUsers', function(users) {
 });
 
 socket.on('prompt', function(prompt) {
-  // prompt is a string
+    console.log(prompt);
+    // prompt is a string
+    story.append("<h1>" + prompt + "</h1></br></br>")
 });
 
-socket.on('story', function(story) {
+socket.on('story', function(fullStory) {
   // [
   //   update: {
   //     user: {
@@ -54,72 +78,77 @@ socket.on('story', function(story) {
 });
 
 socket.on('storyUpdate', function(update) {
+    
+    console.log(update);
+    
+    story.append("<p color='" + update.textColor + "'>" + update.body + "</p>");
+    
   // addition to the story - append to the end
   // {
-  //   user: {
-  //     username: 'username',
-  //     textColor: '#ffffff'
-  //   },
+  //   textColor: "#ffffff",
   //   body: 'this is an update'
   // }
 });
 
 socket.on('storyProgress', function(progress) {
+    console.log(progress);
+    storyProgress.text(progress);
+  // Constant update
   // {
-  //   user: {
-  //     username: 'username',
-  //     textColor: '#ffffff'
-  //   },
+  //   textColor: "#ffffff",
   //   body: 'this is the next update'
   // }
 });
 
+function startTimer(textColor) {
+    console.log(textColor);
+    var barOverlay = $("#loading-bar-overlay");
+    
+    $("#loading-bar").css("border-color", textColor);
+    barOverlay.removeClass("loading-timer");
+    barOverlay.addClass("loading-timer");
+}
 
-socket.on('beginTurn', function(beginTurn) {
-  // User's turn begins - start countdown
+socket.on('userTurn', function(userTurn) {
+    console.log(userTurn);
+    // userTurn is a string, username of the user's whose turn is beginning
+    for(i in userData) {
+        if(userTurn == userData[i].username) {
+            startTimer(userData[i].textColor);
+        }
+    }
+    storyContainer.append("<textarea id='storyTextArea'></textarea>")
 });
 
 socket.on('endTurn', function(endTurn) {
+    console.log(endTurn);
   // User's turn ends - send back text
-})
-
-socket.on('userTurn', function(userTurn) {
-  // userTurn is a string, username of the user's whose turn is beginning
+    for(i in userData) {
+        if(endTurn == userData[i].username) {
+            var text = $("#storyTextArea").val();
+            $("#storyTextArea").remove();
+            socket.emit("storyUpdate", text);
+        } else {
+            socket.emit("storyUpdate", storyProgress);
+            storyProgress.text('');
+        }
+    }
 });
+
 
 $(document).ready(function() {
 
-
+    function login() {
+        socket.emit("login", "1234");
+    }
+    
     function test() {
         var overlay = document.getElementById('loading-overlay');
         overlay.style.height = "100%";
     }
 
-    // ajax call instead of false data
-    var userData = [
-        {name:"Cole Clifford", color:""},
-        {name:"Joseph Roque", color:""},
-        {name:"Bob Dylan", color:""},
-        {name:"Tim Teabow", color:""},
-        {name:"Bruce Lee", color:""}
-    ],
-        color = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#9E9E9E', '#607D8B'],
-        endingSVGText = " height='24' width='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'><path d='M0 0h24v24H0z' fill='none'></path></path></svg>",
-        sideBar = $('#side-bar');
 
-    for(i=0; i<userData.length; i++) {
-        var randomNumForColor = Math.random() * 17,
-            userColor = color[Math.floor(randomNumForColor)],
-            text = "<div class='name-tag'> <svg fill=" + userColor + endingSVGText + " <p class='name' style='color:" + userColor + "'>" + userData[i].name + "</p></div>";
-
-        userData[i].color = userColor;
-        sideBar.append(text);
-
-    }
-
-    $('#loading-bar').css('border-color', userData[0].color);
-
-    setTimeout(test, 1000);
+    setTimeout(login, 1000);
 
 
 });
