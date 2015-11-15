c2042d4f4ae6f2240347e688dec0e5a6d3b135eb
Joseph Roque <contact@josephroque.ca>
2015-11-15 01:55:08 -0500

    Created basic chat app

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
+var socket = io();
+
+$('form').submit(function(){
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
+});
+
+socket.on('chat message', function(msg){
+  $('#messages').append($('<li>').text(msg));
+});
+
 $(document).ready(function() {
-
-  // Place JavaScript code here...
-
 });
